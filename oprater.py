aa08d43724e719313987d0b9bd332d0ab7838495
salamnoor3689-collab <salamnoor3689@gmail.com>
2025-12-31 08:47:16 +0530

    Add files via upload

---
+++ oprater.py
@@ -0,0 +1,34 @@
+x="20"
+print(x)
+
+a=5
+print(-a)
+
+p=10
+q=5
+print(p>q)
+print(p==q)
+
+x="true"
+y="false"
+print(x and y)
+print(x or y)
+print(not x)
+
+is_pass ="true"
+print(is_pass)   
+
+a=5
+b=3
+print(a&b)
+print(a|b)
+
+numbers=[1,2,3,4,6]
+print(2 in numbers)
+print(5 not in numbers)
+
+
+x=10
+y=10
+print(x is y)
+print(x is not y)
