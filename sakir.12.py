ba274f39781bcdb1a7f30db285c1de1cb6978893
salamnoor3689-collab <salamnoor3689@gmail.com>
2026-01-05 11:24:27 +0530

    Add files via upload

---
+++ sakir.12.py
@@ -0,0 +1,37 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x="tomorrow"
+print (x)
+tomorrow
+x=35
+print (x)
+35
+x=79.90
+print (x)
+79.9
+x=5h
+SyntaxError: invalid decimal literal
+x=3h
+SyntaxError: invalid decimal literal
+x=3j
+print (x)
+3j
+x=["mic","speaker","phone"]
+print (x)
+['mic', 'speaker', 'phone']
+x=("mobile","laptop","p.c")
+print (x)
+('mobile', 'laptop', 'p.c')
+x=("name":"joy","age":45)
+SyntaxError: invalid syntax
+x={"name":"joy","age":45}
+print (x)
+{'name': 'joy', 'age': 45}
+x=True
+print (x)
+True
+x=
+SyntaxError: invalid syntax
+x=None
+print (x)
+None
